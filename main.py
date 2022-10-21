20f62a8deef47cce24ee9ab849caae26bcedf3b3
thomaszdxsn <thomaszdxsn@icloud.com>
2022-10-21 22:43:42 +0800

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from textual.app import App
 from textual.reactive import Reactive
+from rich.align import Align
 from textual.widgets import Footer, Placeholder, Static
+from textual.widget import Widget
+
+
+class TextWidget(Widget):
+
+    def __init__(self, text: str, *, name: str=None):
+        self.text = text
+        super().__init__(name=name)
+
+    def render(self):
+        return  Align.center(self.text, vertical="middle")
 
 
 class HelloWorld(App):
@@ -8,9 +20,8 @@ class HelloWorld(App):
 
     async def on_mount(self) -> None:
         """Build layout here."""
-        bar = Placeholder(name="Hello World")
 
-        await self.view.dock(bar, z=1)
+        await self.view.dock(TextWidget("Hello World"))
 
 
 HelloWorld.run(log="textual.log", log_verbosity=2)
